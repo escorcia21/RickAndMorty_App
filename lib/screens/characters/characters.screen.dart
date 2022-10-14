f6a4b24bf764e5d93aaa139effd1462f9ac0bad2
escorcia21 <carlosescorcia21@hotmail.com>
2022-10-14 13:42:12 -0500

    Infinite scroll

--- lib/screens/characters/characters.screen.dart
+++ lib/screens/characters/characters.screen.dart
@@ -24,6 +24,39 @@ class _CharactersSreenState extends State<CharactersSreen>
     with AutomaticKeepAliveClientMixin {
   /// The list of characters
   late Future<List<Character>> _characters;
+  final _controller = ScrollController();
+  var pagination = 2;
+
+  /// Loading more characters when the user scrolls to the bottom of the list
+  void _loadMoreCharacters() async {
+    // if the pagination is more than 41, then stop loading more characters
+    if (pagination > 41) {
+      // Show a snackbar to notify the user that there are no more characters
+      const snackBar = SnackBar(content: Text('No more characters'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      // Stop the listener
+      _controller.dispose();
+      return;
+    }
+
+    // Fetch the next page of characters
+    try {
+      final page = await RickAndMortyService.getCharacters(page: pagination);
+
+      // Add the new characters to the list and increment the pagination
+      setState(() {
+        pagination++;
+        _characters = _characters.then((characters) {
+          return [...characters, ...page];
+        });
+      });
+    } catch (error) {
+      // Show a snackbar to notify the user that there was an error
+      final snackBar = SnackBar(content: Text(error.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   void initState() {
@@ -33,10 +66,30 @@ class _CharactersSreenState extends State<CharactersSreen>
       Showing a loading indicator while fetching the data
       and showing an error message with a SnackBar if something went wrong
     */
-    _characters = RickAndMortyService.getCharacters().catchError((error) {
+    try {
+      _characters = RickAndMortyService.getCharacters();
+    } catch (error) {
+      // Show a snackbar to notify the user that there was an error
       final snackBar = SnackBar(content: Text(error.toString()));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+    /*
+      Adding a listener to the controller to load more characters when scrolling
+      to the bottom of the list    
+    */
+    _controller.addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        _loadMoreCharacters();
+      }
     });
+  }
+
+  // When the widget is removed from the widget tree, dispose the controller
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -53,11 +106,20 @@ class _CharactersSreenState extends State<CharactersSreen>
 
         return ListView.builder(
           physics: const ScrollPhysics(parent: BouncingScrollPhysics()),
-          itemCount: snapshot.data.length,
+          itemCount: snapshot.data.length + 1,
+          controller: _controller,
           itemBuilder: (BuildContext context, int index) {
             // Get the character from the snapshot and creating a CharacterCard
-            Character character = snapshot.data[index];
-            return CharacterCard(character: character);
+            if (index < snapshot.data.length) {
+              final character = snapshot.data[index];
+              return CharacterCard(character: character);
+            }
+
+            // Show a loading indicator when the user are in the end of the list
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 32),
+              child: Center(child: CircularProgressIndicator()),
+            );
           },
         );
       },
