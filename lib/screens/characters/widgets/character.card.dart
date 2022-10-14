042d24924504cca1d35dfa2d8794dc3e3431b4a6
escorcia21 <carlosescorcia21@hotmail.com>
2022-10-14 12:27:44 -0500

    Navigation to character details

--- lib/screens/characters/widgets/character.card.dart
+++ lib/screens/characters/widgets/character.card.dart
@@ -1,3 +1,4 @@
+import 'package:rickandmorty/screens/characters/character.details.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:rickandmorty/models/character.dart';
 import 'package:rickandmorty/theme/colors.dart';
@@ -30,62 +31,72 @@ class CharacterCard extends StatelessWidget {
     return InkWell(
       // Navigate to the details screen
       onTap: () {
-        SnackBar snackBar = SnackBar(content: Text(character.name));
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.push(context, _createRoute());
       },
 
       // The card itself
-      child: Card(
-        child: Row(children: [
-          Image.network(
-            character.image,
-            width: 150,
-            height: 150,
+      child: AnimatedBuilder(
+        animation: Tween(begin: 0.0, end: 1.0).animate(
+          CurvedAnimation(
+            parent: const AlwaysStoppedAnimation(0),
+            curve: Curves.easeIn,
           ),
-          const SizedBox(
-            width: 10,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Show the character name and status
-                Text(
-                  character.name,
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                Row(
+        ),
+        builder: (BuildContext context, Widget? child) {
+          return Card(
+            child: Row(children: [
+              Image.network(
+                character.image,
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(width: 12, height: 12, decoration: coloredDot),
-                    const SizedBox(width: 8.0),
+                    // Show the character name and status
                     Text(
-                      '${character.status} - ${character.species}',
+                      character.name,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            width: 12, height: 12, decoration: coloredDot),
+                        const SizedBox(width: 8.0),
+                        Text(
+                          '${character.status} - ${character.species}',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        )
+                      ],
+                    ),
+
+                    // Show the character's origin and Last known location
+                    Text(
+                      'Last known location:',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    Text(
+                      character.location.name,
                       style: Theme.of(context).textTheme.subtitle2,
-                    )
+                    ),
+                    Text(
+                      'Origin:',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    Text(
+                      character.origin.name,
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
                   ],
                 ),
-
-                // Show the character's origin and Last known location
-                Text(
-                  'Last known location:',
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-                Text(
-                  character.location.name,
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                Text(
-                  'Origin:',
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-                Text(
-                  character.origin.name,
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-              ],
-            ),
-          ),
-        ]),
+              ),
+            ]),
+          );
+        },
       ),
     );
   }
@@ -99,5 +110,29 @@ class CharacterCard extends StatelessWidget {
     } else {
       return Colors.grey;
     }
+  }
+
+  // Router to the details screen with a transition animation
+  Route _createRoute() {
+    return PageRouteBuilder(
+      // The page to navigate to
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          CharacterDetailScreen(character: character),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        // Animate the position of the child
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        // Slide the page from bottom to top while fading it in
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
   }
 }
