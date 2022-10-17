57372250057952e0cf2c5abc005af2ac96dff25d
escorcia21 <carlosescorcia21@hotmail.com>
2022-10-17 13:56:10 -0500

    landscape error

--- lib/screens/episodes/episode.detail.screen.dart
+++ lib/screens/episodes/episode.detail.screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:rickandmorty/models/character.dart';
 import 'package:rickandmorty/models/episode.dart';
@@ -41,63 +43,76 @@ class _EpisodeDetailScreenState extends State<EpisodeDetailScreen> {
       appBar: AppBar(
         title: Text(widget.episode.name),
       ),
-      body: Column(
-        children: [
-          // Show the name, type and dimension of the location
-          Image.network(
-            widget.episode.stillPath,
-            width: double.infinity,
-            fit: BoxFit.fill,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            widget.episode.airDate,
-            style: Theme.of(context).textTheme.displayLarge,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              widget.episode.overview,
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+              height: max(
+                  500,
+                  MediaQuery.of(context).orientation == Orientation.portrait
+                      ? MediaQuery.of(context).size.height -
+                          AppBar().preferredSize.height
+                      : MediaQuery.of(context).size.width)),
+          child: Column(
+            children: [
+              // Show the name, type and dimension of the location
+              Image.network(
+                widget.episode.stillPath,
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                widget.episode.airDate,
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.episode.overview,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
 
-          // Show the list of characters that appear in the episode
-          Text(
-            'Characters: ${widget.episode.characters.length}',
-            style: Theme.of(context).textTheme.displayLarge,
-          ),
-          Expanded(
-            child: FutureBuilder(
-              future: _characters,
-              builder: (context, snapshot) {
-                // If the data is not fetched yet, show a loading indicator
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+              // Show the list of characters that appear in the episode
+              Text(
+                'Characters: ${widget.episode.characters.length}',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              Expanded(
+                child: FutureBuilder(
+                  future: _characters,
+                  builder: (context, snapshot) {
+                    // If the data is not fetched yet, show a loading indicator
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
 
-                // If the list of characters is loaded, show the list of characters
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                    itemCount: widget.episode.characters.length,
-                    itemBuilder: (context, index) {
-                      Character character = snapshot.data![index];
-                      return CharacterCard(character: character);
-                    },
-                  ),
-                );
-              },
-            ),
+                    // If the list of characters is loaded, show the list of characters
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                        primary: false,
+                        shrinkWrap: true,
+                        itemCount: widget.episode.characters.length,
+                        itemBuilder: (context, index) {
+                          Character character = snapshot.data![index];
+                          return CharacterCard(character: character);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
